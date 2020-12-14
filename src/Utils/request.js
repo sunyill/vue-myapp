d46f985219a1fe2e753356cafb819e4d9702e642
azan <sunyill0707@163.com>
2020-12-14 14:14:49 +0800

    4.配置json-bigint,request.js处理大数字

--- src/Utils/request.js
+++ src/Utils/request.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
+import jsonBigInt from 'json-bigint'
 
 const instance = axios.create({
   baseUrl: '/',
   timeout: '1000'
 })
+
+// 处理id大数据
+instance.defaults.transformResponse = [function (data) {
+  try {
+    return jsonBigInt.parse(data)
+  } catch (error) {
+    return data
+  }
+}
+
+]
 // 添加拦截器
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
@@ -16,6 +28,7 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+  console.log('数据返回', response)
   // Any status code that lie within the range of 2xx cause this function to trigger
   // Do something with response data
   return response
